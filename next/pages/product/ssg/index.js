2d3968c8f9464d80b1ca8eba755a76dd898c3a1e
Code Like Pro <ma@MAs-MacBook-Pro.local>
2022-09-17 19:10:38 +0200

    ssg fetching

---
+++ next/pages/product/ssg/index.js
@@ -0,0 +1,27 @@
+import { sanityClient } from '../../../utils/sanity/client';
+import Link from 'next/link';
+
+const Products = ({ products }) => {
+  return (
+    <div>
+      {products.map((product) => (
+        <Link href={`/product/ssg/${product.slug}`} key={product._id}>
+          <a>
+            <p>{product.title}</p>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Products;
+
+export const getStaticProps = async () => {
+  const products = await sanityClient.fetch(
+    '*[_type=="product" && defined(slug.current) && !(_id in path("drafts.**"))]{..., "slug":slug.current}'
+  );
+  return {
+    props: { products },
+  };
+};
